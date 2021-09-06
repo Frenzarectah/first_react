7da753df124791c24b9f98264de754daa1386754
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-09-06 20:48:00 +0200

    [creating] -a new react app to learn about

---
+++ src/Other.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Other extends React.Component{
+    render(){
+        return(
+            <p>Questo è il componente other scritto con la classe</p> 
+    )};
+};
+export default Other;
